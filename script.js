9b34a97b2919b16f45f8839db6d5d4b127edaa45
Veer Gosai <71587878+VeerGosai@users.noreply.github.com>
2023-08-19 19:30:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 // script.js
 
-const changeContentButton = document.getElementById('changeContent');
-const mainContent = document.querySelector('main p');
-
-changeContentButton.addEventListener('click', () => {
+// Function to change the content of the main area
+function changeMainContent() {
+    const mainContent = document.querySelector('main p');
     mainContent.textContent = 'Content has been changed dynamically!';
+}
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+}
+
+// Function to display an alert
+function showAlert() {
+    alert('This is an alert!');
+}
+
+// Event listeners for buttons
+document.addEventListener('DOMContentLoaded', () => {
+    const changeContentButton = document.getElementById('changeContent');
+    const toggleDarkModeButton = document.getElementById('toggleDarkMode');
+    const showAlertButton = document.getElementById('showAlert');
+
+    changeContentButton.addEventListener('click', changeMainContent);
+    toggleDarkModeButton.addEventListener('click', toggleDarkMode);
+    showAlertButton.addEventListener('click', showAlert);
 });
